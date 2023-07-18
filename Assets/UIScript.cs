5942319c029ae3b3f7eca3cb0701676df30892f3
yiyeuniverse <wizardgang@gmail.com>
2023-07-18 16:56:34 +0100

    Basic UI script

--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
+    public GameObject pointer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        changePointerStyle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pointer.transform.position = (Input.mousePosition);
+    }
+
+    void changePointerStyle()
+    {
+        return;
     }
 }
